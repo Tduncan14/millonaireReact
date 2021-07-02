5534491fabfe3cae7d87374cbdbf0a267ad09d61
tduncan14 <nbatre1@yahoo.com>
2021-07-01 22:38:21 -0700

    keep moving forward

--- millonaire/src/App.js
+++ millonaire/src/App.js
@@ -1,12 +1,53 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 function App() {
+
+
+  const moneyPyramid = [
+    { id: 1, amount: '$100'},
+    { id: 2, amount: '$200'},
+     {id: 3, amount: '$300'},
+     { id: 4, amount: '$500'},
+     { id: 5, amount: '$1000'},
+      {id: 6, amount: '$2000'},
+      {id: 7, amount: '$4000'},
+     { id: 8, amount: '$8000'},
+     {id: 9, amount: '$16000'},
+      {id: 10, amount: '$32000'},
+      {id: 11, amount: '$64000'},
+      {id: 12, amount: '$12500'},
+      {id: 13, amount: '$250000'},
+     { id: 14, amount: '$50000'},
+      {id: 15, amount: '$1000000'}
+      
+
+  ]
+
+
   return (
     <div className="App">
 
-      Quizs App
-      
+    <div className="main">
+       main
+    </div>
+
+    <div className='pyramid'>
+      money
+
+      <ul className="moneylist">
+        {moneyPyramid.map(q=>(
+          <li className="moneylistitem">
+              <span className="moneyListNumber">{q.id}</span>
+              <span className="moneyListAmount">{q.amount}</span>
+          </li>
+
+
+        ))}
+
+      </ul>
+    </div>
+    
     </div>
   );
 }
