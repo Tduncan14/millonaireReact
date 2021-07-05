752b3dbd07ce4cb9ba9c04b29727200aac8675a2
tduncan14 <nbatre1@yahoo.com>
2021-07-05 12:58:31 -0700

    keep moving forward

--- millonaire/src/App.js
+++ millonaire/src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState} from 'react'
+import { isValidElement, useEffect, useMemo, useState} from 'react'
 import Triva from './components/Trivia';
+import Timer from './components/Timer';
 
 function App() {
 
@@ -13,6 +14,32 @@ function App() {
   const [stop,setStop] = useState(false)
 
 
+  const moneyPyramid = useMemo(()=>(
+    [
+      { id: 1, amount: '$ 100'},
+      { id: 2, amount: '$ 200'},
+       {id: 3, amount: '$ 300'},
+       { id: 4, amount: '$ 500'},
+       { id: 5, amount: '$ 1000'},
+        {id: 6, amount: '$ 2000'},
+        {id: 7, amount: '$ 4000'},
+       { id: 8, amount: '$ 8000'},
+       {id: 9, amount: '$ 16000'},
+        {id: 10, amount: '$ 32000'},
+        {id: 11, amount: '$ 64000'},
+        {id: 12, amount: '$ 12500'},
+        {id: 13, amount: '$ 250000'},
+       { id: 14, amount: '$ 50000'},
+        {id: 15 , amount: ' $ 1000000'}
+        
+  
+    ].reverse()
+    
+
+
+  ),[])
+
+    
   const data = [
     {
       id: 1,
@@ -85,25 +112,7 @@ function App() {
 
 
 
-  const moneyPyramid = [
-    { id: 1, amount: '$ 100'},
-    { id: 2, amount: '$ 200'},
-     {id: 3, amount: '$ 300'},
-     { id: 4, amount: '$ 500'},
-     { id: 5, amount: '$ 1000'},
-      {id: 6, amount: '$ 2000'},
-      {id: 7, amount: '$ 4000'},
-     { id: 8, amount: '$ 8000'},
-     {id: 9, amount: '$ 16000'},
-      {id: 10, amount: '$ 32000'},
-      {id: 11, amount: '$ 64000'},
-      {id: 12, amount: '$ 12500'},
-      {id: 13, amount: '$ 250000'},
-     { id: 14, amount: '$ 50000'},
-      {id: 15 , amount: ' $ 1000000'}
-      
-
-  ].reverse()
+  
 
   useEffect(() => {
 
@@ -124,7 +133,7 @@ function App() {
 <div className="top">
 
 <div class="timer">
-   30
+   <Timer  setStop={setStop} questionNumber={questionNumber} />
 </div>
 
 </div>
